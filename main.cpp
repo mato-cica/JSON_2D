9f62e1e595ab52085eac337c85893d948725fa98
mato-cica <mato.cica@gmail.com>
2016-04-04 18:34:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include "Read_JSON.h"
-#include "Parse_2D.h"
+#include "Parse_JSON.h"
 #include "Write_JSON.h"
-#include <map>
 using namespace std;
 
 int main()
@@ -12,15 +11,17 @@ int main()
     {
         Read_JSON from_file;
         from_file.write_JSON_string();
-        Parse_2D First_Row;
-        First_Row.take_1st_row(from_file.getJSON_string());
-        First_Row.make_key();
-        First_Row.take_all_rows(from_file.getJSON_string());
-        First_Row.array_from_JSON_string();
-        First_Row.arranged_array_from_JSON_string();
-        First_Row.write_coordinates_of_9();
-        First_Row.write_sum_of_coordinates();
-        cout << "Any key beside 'y' for exit..." << endl;
+        Parse_JSON parse_JSON;
+        parse_JSON.take_1st_row(from_file.get_str_JSON_2D());
+        parse_JSON.make_arrangement_key();
+        parse_JSON.set_Number_Of_Columns();
+        parse_JSON.take_all_rows(from_file.get_str_JSON_2D());
+        Write_JSON write_2D;
+        write_2D.array_from_JSON_string(parse_JSON.get_mapAllRows(), parse_JSON.get_Number_Of_Columns());
+        write_2D.arranged_array_from_JSON_string(parse_JSON.get_mapArranged2Darray(), parse_JSON.get_Number_Of_Columns());
+        write_2D.write_coordinates_of_9(parse_JSON.get_map9coordinates(), parse_JSON.get_Number_Of_Columns());
+        write_2D.write_sum_of_coordinates();
+        cout << " Any key besides 'y' for exit..." << endl;
         cin >> yn;
     }
 
