cff657987f651b7f02d1e880475dd2431c3c683f
mato-cica <mato.cica@gmail.com>
2016-04-04 18:36:39 +0200

    Update Read_JSON.h

--- Read_JSON.h
+++ Read_JSON.h
@@ -8,13 +8,10 @@ class Read_JSON
     public:
         Read_JSON();
         void write_JSON_string();
-        string getJSON_string();
-        //string m_str_JSON_2D;
+        string &get_str_JSON_2D();
     protected:
     private:
         string m_str_JSON_2D;
-        //string m_JSON_file;
-
 };
 
 #endif // READ_JSON_H
